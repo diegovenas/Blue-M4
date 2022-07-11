2487e70a5be3cb27028bae736dd21d23bad5e705
diegovenas <diegovenas61@gmail.com>
2022-07-11 20:39:45 -0300

    API OPERATION

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('Users')
     .addTag('Products')
+    .addTag('Tables')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
